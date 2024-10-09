6a9710c01652417034828c47b90a5de9a433dd4f
Aung Myo Min <70466675+aungmyominoffical@users.noreply.github.com>
2024-10-09 13:48:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
  wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-354.0.0-linux-arm.tar.gz
  tar -zxvf google-cloud-sdk-354.0.0-linux-arm.tar.gz
+ cd google-cloud-sdk/bin/
