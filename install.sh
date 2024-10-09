285c9569445195f44dd84ee6c946c6f189ed4569
Aung Myo Min <70466675+aungmyominoffical@users.noreply.github.com>
2024-10-09 14:34:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+ rm -rf google-cloud-sdk-354.0.0-linux-arm.tar.gz
+ rm -rf google-cloud-sdk
+ sudo apt install openssh-server
  wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-354.0.0-linux-arm.tar.gz
  tar -zxvf google-cloud-sdk-354.0.0-linux-arm.tar.gz
- wget https://raw.githubusercontent.com/aungmyominoffical/gcp/refs/heads/main/.ssh/google_compute_engine
- wget https://raw.githubusercontent.com/aungmyominoffical/gcp/refs/heads/main/.ssh/google_compute_engine.pub
+ rm -rf .ssh
  mkdir -p .ssh
+ wget https://raw.githubusercontent.com/aungmyominoffical/gcp/refs/heads/main/.ssh/google_compute_engine
+  mv google_compute_engine ./.ssh/google_compute_engine
+ wget https://raw.githubusercontent.com/aungmyominoffical/gcp/refs/heads/main/.ssh/google_compute_engine.pub
  mv google_compute_engine.pub ./.ssh/google_compute_engine.pub
- mv google_compute_engine.pub ./.ssh/google_compute_engine
- # cd google-cloud-sdk/bin/
+ cd google-cloud-sdk/bin/
+ gcloud auth login --no-launch-browser
+ gcloud cloud-shell ssh
